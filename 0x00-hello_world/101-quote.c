e1a318197c5e00c34a244b68c1ba249dff3d189d
Marioss0 <marioatef8714@gmail.com>
2023-07-15 07:52:07 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  A c program that prints a line to the standard error
+ *
+ * Return 1 (Not Success)
+ *
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+
+	Return (1);
+}
