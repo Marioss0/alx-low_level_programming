3bf0e6d29f0936d48e6ece6fc9bd66a90a78244b
Marioss0 <marioatef8714@gmail.com>
2023-07-20 19:59:31 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A Fizz Buzz Program
+ *
+ * Return: fizz Buzz
+*/
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if ((count % 3 == 0) && !(count % 15 == 0))
+			printf("Fizz");
+		else if ((count % 5 == 0) && !(count % 15 == 0))
+			printf("Buzz");
+		else if (count % 15 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%i", count);
+		if (count != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
